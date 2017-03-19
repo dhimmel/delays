9e76ccd7f6358892d85c42bdc26ddb2c400e34d0
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-03-19 13:57:36 -0400

    Move functions to pydelays.
    Extract new PubDate and EPubDate fields

---
+++ pydelays/eutilities.py
@@ -0,0 +1,121 @@
+import os
+import time
+import collections
+import logging
+
+import requests
+import lxml.etree
+import tqdm
+
+
+def esearch_query(payload, retmax=10000, sleep=0.34, tqdm=tqdm.tqdm):
+    """
+    Return identifiers using the ESearch E-utility.
+
+    Set `tqdm=tqdm.notebook` to use the tqdm notebook interface.
+    """
+    url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
+    payload['rettype'] = 'xml'
+    payload['retmax'] = retmax
+    payload['retstart'] = 0
+    ids = list()
+    count = 1
+    progress_bar = None
+    while payload['retstart'] < count:
+        response = requests.get(url, params=payload)
+        tree = lxml.etree.fromstring(response.content)
+        count = int(tree.findtext('Count'))
+        if not progress_bar:
+            progress_bar = tqdm(total=count, unit='ids')
+        add_ids = [id_.text for id_ in tree.findall('IdList/Id')]
+        ids += add_ids
+        payload['retstart'] += retmax
+        progress_bar.update(len(add_ids))
+        time.sleep(sleep)
+    progress_bar.close()
+    return id
+
+
+def pubmed_esummary(ids, write_file, retmax=100, retmin=20, sleep=0.34,
+                    error_sleep=10, tqdm=tqdm.tqdm):
+    """
+    Submit an ESummary query for PubMed records and write results as xml
+    to write_file.
+
+    Set `tqdm=tqdm.notebook` to use the tqdm notebook interface.
+    """
+    # Base URL for PubMed's esummary eutlity
+    url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi'
+
+    # Set up progress stats
+    n_total = len(ids)
+    successive_errors = 0
+    progress_bar = tqdm(total=n_total, unit='articles')
+
+    # Write first line of XML
+    write_file.write('<eSummaryResult>\n')
+
+    # Set up queue
+    idq = collections.deque()
+    for i in range(0, len(ids), retmax):
+        idq.append(ids[i:i+retmax])
+
+    # Query until the queue is empty
+    while idq:
+        time.sleep(sleep)
+        id_subset = idq.popleft()
+        id_subset_len = len(id_subset)
+
+        # Perform eutilities API request
+        id_string = ','.join(map(str, id_subset))
+        payload = {'db': 'pubmed', 'id': id_string, 'rettype': 'xml'}
+        try:
+            response = requests.get(url, params=payload)
+            tree = lxml.etree.fromstring(response.content)
+            successive_errors = 0
+        except Exception as e:
+            successive_errors += 1
+            logging.warning(
+                f'{successive_errors} successive error: {id_subset_len} IDs'
+                f'[{id_subset[0]} … {id_subset[-1]}] threw {e}'
+            )
+            if id_subset_len >= retmin * 2:
+                mid = len(id_subset) // 2
+                idq.appendleft(id_subset[:mid])
+                idq.appendleft(id_subset[mid:])
+            else:
+                idq.appendleft(id_subset)
+            time.sleep(error_sleep * successive_errors)
+            continue
+
+        # Write XML to file
+        for docsum in tree.getchildren():
+            xml_str = lxml.etree.tostring(docsum, encoding='unicode')
+            write_file.write(xml_str.rstrip() + '\n')
+
+        # Report progress
+        progress_bar.update(id_subset_len)
+
+    progress_bar.close()
+    # Write final line of XML
+    write_file.write('</eSummaryResult>\n')
+
+
+def create_test_xml():
+    """
+    Run to recreate esummary_journal-articles_test.xml
+    """
+    directory = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(directory,
+                        'test_data', 'esummary_journal-articles_test.xml')
+    pubmed_ids = [
+        '27094199',  # Circ Cardiovasc Genet
+        '26158728',  # PLoS Comput Biol
+        '25648772',  # PeerJ
+        '21736753',  # BioData Min
+        '25915600',  # Nature Genet
+        '20081222',  # Bioinformatics
+    ]
+    with open(path, 'wt') as write_file:
+        pubmed_esummary(pubmed_ids, write_file)
+    return path
---
+++ pydelays/xml_to_dates.py
@@ -0,0 +1,132 @@
+import datetime
+import mimetypes
+import importlib
+import logging
+import itertools
+
+import tqdm
+from lxml import etree
+
+
+encoding_to_module = {
+    'gzip': 'gzip',
+    'bzip2': 'bz2',
+    'xz': 'lzma',
+}
+
+
+def iterparse(path):
+    """
+    First yield the ElementTree root, then yield elements from an XML file.
+    """
+    # Automatically detect compression
+    type_, encoding = mimetypes.guess_type(path)
+    if encoding is None:
+        opener = open
+    else:
+        module = encoding_to_module[encoding]
+        opener = importlib.import_module(module).open
+
+    # Open file and yield from the element tree
+    with opener(path, 'rb') as read_file:
+        context = etree.iterparse(read_file, events=('start', 'end'))
+        yield next(context)[1]
+        yield from (elem for event, elem in context if event == 'end')
+
+
+def parse_date_text(text):
+    """
+    Parse an `eSummaryResult/DocSum/Item[@Name='History']/Item[@Type='Date']`
+    element.
+    The time on the date is discarded. A `datetime.date` object is returned
+    """
+    date_, time_ = text.split(' ')
+    date_tuple = date_.split('/')
+    year, month, day = map(int, date_tuple)
+    return datetime.date(year, month, day)
+
+
+def parse_pubdate_text(text):
+    """
+    Parse the text contained by the following elements:
+
+    `eSummaryResult/DocSum/Item[@Name='PubDate' @Type='Date']`
+    `eSummaryResult/DocSum/Item[@Name='EPubDate' @Type='Date']`
+
+    See https://www.nlm.nih.gov/bsd/licensee/elements_article_source.html
+    A `datetime.date` object is returned or `None` if the date is incomplete
+    or corrupted.
+    """
+    try:
+        return datetime.datetime.strptime(text, '%Y %b %d').date()
+    except ValueError:
+        return None
+
+
+def parse_esummary_history(docsum):
+    """docsum is an xml Element."""
+    # Extract all historical dates
+    date_pairs = list()
+    seen = set()
+    for item in docsum.findall("Item[@Name='History']/Item[@Type='Date']"):
+        name = item.get('Name')
+        try:
+            date_ = parse_date_text(item.text)
+        except Exception as e:
+            pubmed_id = int(docsum.findtext('Id'))
+            msg = (f'article {pubmed_id}; name: {name}; '
+                   f'date: {item.text}, threw: {e}')
+            logging.warning(msg)
+            continue
+
+        date_pair = name, date_
+        if date_pair in seen:
+            continue
+        seen.add(date_pair)
+        date_pairs.append(date_pair)
+    date_pairs.sort(key=lambda x: x[0])
+    history = dict()
+    for name, group in itertools.groupby(date_pairs, key=lambda x: x[0]):
+        for i, (name, date_) in enumerate(group):
+            history[f'{name}_{i}'] = date_
+    return history
+
+
+def parse_esummary(elem):
+    """
+    Extract pubmed, journal, and date information from an eSummaryResult/DocSum
+    """
+    article = dict()
+    article['pubmed_id'] = int(elem.findtext('Id'))
+    article['journal_nlm_id'] = elem.findtext("Item[@Name='NlmUniqueID']")
+    for key, name in ('pub', 'PubDate'), ('epub', 'EPubDate'):
+        xpath = f"Item[@Name='{name}'][@Type='Date']"
+        text = elem.findtext(xpath)
+        article[key] = parse_pubdate_text(text)
+    article.update(parse_esummary_history(elem))
+    return article
+
+
+def extract_articles_from_esummaries(path, n_articles=None, tqdm=tqdm.tqdm):
+    """
+    Extract a list of articles (dictionaries with date information) from a
+    an eSummaryResult XML file. Specify `n_articles` to enable a progress bar.
+    """
+    if n_articles is not None:
+        progress_bar = tqdm(total=n_articles, unit='articles')
+
+    parser = iterparse(path)
+    root = next(parser)
+    articles = list()
+    for elem in parser:
+        if elem.tag != 'DocSum':
+            continue
+        article = parse_esummary(elem)
+        articles.append(article)
+        if n_articles is not None:
+            progress_bar.update(1)
+        root.clear()  # Reset element to free memory
+
+    if n_articles is not None:
+        progress_bar.close()
+    return articles
